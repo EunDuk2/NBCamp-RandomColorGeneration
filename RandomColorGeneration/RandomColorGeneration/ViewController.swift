54b29e7b7e475a5edc7b4b1d8c919d8d69150ed7
Eunsung <dmsqls6789@naver.com>
2025-02-19 16:42:59 +0900

    Step2: 랜덤 색상 생성 기능 구현

--- RandomColorGeneration/RandomColorGeneration/ViewController.swift
+++ RandomColorGeneration/RandomColorGeneration/ViewController.swift
@@ -8,12 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var viewRgb: UIView!
+    @IBOutlet weak var labelRgb: UILabel!
+    
+    var randomRed: CGFloat = 1
+    var randomGreen: CGFloat = 1
+    var randomBlue: CGFloat = 1
+    var randomColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1.0)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    func setRandomColor() {
+        randomRed = CGFloat.random(in: 0...1)
+        randomGreen = CGFloat.random(in: 0...1)
+        randomBlue = CGFloat.random(in: 0...1)
+        randomColor = UIColor(red: randomRed, green: randomGreen, blue: randomBlue, alpha: 1.0)
+    }
 
-
+    @IBAction func changeColor(_ sender: Any) {
+        setRandomColor()
+        viewRgb.backgroundColor = randomColor
+        labelRgb.text = "R: \(Int(randomRed * 255)), G: \(Int(randomGreen * 255)), B: \(Int(randomBlue * 255))"
+    }
+    
 }
 
